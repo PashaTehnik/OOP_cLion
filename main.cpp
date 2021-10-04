b1c2c796d07646726036cc2500a2140e0b79b0ac
Pasha_Tehnik <polovinko.p0112@gmail.com>
2021-10-04 17:29:23 +0700

    dont work

--- main.cpp
+++ main.cpp
@@ -2,9 +2,8 @@
 #include <fstream>
 #include <cstdlib>
 #include <list>
-#include <iterator>
 #include <map>
-#include <cstring>
+
 using namespace std;
 
 list<string> _list;
@@ -21,19 +20,8 @@ bool comp_str (const std::string& first, const std::string& second)
 {
     return first < second;
 }
-/*
-list <string> distin(list<string> arr){
-    list <string> new_list;
-    for (auto i : arr){
-        for (auto j : arr){
 
-        }
-    }
-    return new_list;
-}
-*/
 int main(int argc, char** argv) {
-
     ifstream inp(argv[1]);
     ofstream out(argv[2]);
 
@@ -50,11 +38,11 @@ int main(int argc, char** argv) {
         int n = str.size();
 
         for (int i = 0; i < n; i++) {
-            if ((str[i] == ' ') || (str[i] == '.') || (str[i] == ',') || (str[i] == ';') || (str[i] == ':') || (str[i] == '\n') || (str[i] == '\0')) {
+            if ((!isalnum(str[i])) || (str[i]=='\n') || (str[i]=='\0')) {
                 book[word] += 1;
                 _list.push_front(word);
-                if((str[i] == '\n') || (str[i] == '\0')) goto next_line;
-                while((str[i] == ' ') || (str[i] == '.') || (str[i] == ',') || (str[i] == ';') || (str[i] == ':')) i++;
+                if(str[i] == '\n') goto next_line;
+                while((!isalnum(str[i])) || (str[i]=='\n') || (str[i]=='\0'))  i++;
                 word = "";
             }
 
@@ -64,22 +52,14 @@ int main(int argc, char** argv) {
     _list.sort(comp_str);
     _list.unique();
     _list.sort(comp);
-    //for (it_list=_list.begin(); it_list != _list.end(); ++it)
-        //cout << it_list->data() << endl;
-        //std::cout << '\n';
-    //distin(_list);
+
     int count_words = 0;
     for (auto v : _list){
         count_words += book[v];
     }
     printf("             Word        freq.  freq(%%)\n");
     for (auto v : _list){
-        printf("%20s    %3d    %4f%%\n", v.c_str(), book[v], ((float)book[v])/((float)count_words));
-
+        printf("%20s    %3d    %4f%%\n", v.c_str(), book[v], ((float)book[v])/((float)count_words)*100);
+        out << v.c_str() << "," << book[v] <<  "," << ((float)book[v])/((float)count_words)*100 << "%\n";
     }
-
-/*
-    for (it = book.begin(); it != book.end(); it++)
-        cout << "key " << it->first << ", value " << it->second << endl;
-*/
- }
+}
